dff5e11d5ee96715be07f61dbbf7bb0be6495e50
ahmed sokar <ahmed.sokar@fawry.com>
2023-05-08 10:14:12 +0300

    Initial Commit

---
+++ NewsPlus/NewsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPlusApp.swift
+//  NewsPlus
+//
+//  Created by admin on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
